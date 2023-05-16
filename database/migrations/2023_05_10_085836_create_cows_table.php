8261c1f15e325de71db21689702a327b3a0f7240
xaiful <saifulbabu787@gmail.com.com>
2023-05-16 10:43:33 +0600

    Resolve conflicts from previous push

--- database/migrations/2023_05_10_085836_create_cows_table.php
+++ database/migrations/2023_05_10_085836_create_cows_table.php
@@ -15,12 +15,21 @@ class CreateCowsTable extends Migration
     {
         Schema::create('cows', function (Blueprint $table) {
             $table->id();
+<<<<<<< HEAD:database/migrations/2023_05_10_085836_create_cows_table.php
             $table->text('name');
             $table->integer('age');
             $table->enum('gender', ['male', 'female']);
             $table->integer('weight');
             $table->string('color');
             $table->string('importer');
+=======
+            $table->string('title');
+            $table->string('slug');
+            $table->text('description');
+            $table->integer('price');
+            $table->text('images');
+            $table->boolean('status')->default(1);
+>>>>>>> parent of 9875aff (First commit):database/migrations/2022_11_23_050024_create_posts_table.php
             $table->timestamps();
         });
     }
